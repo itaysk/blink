d786d550cd19270a3be181f5042dbda7b67cba1b
itaysk <itay@itaysk.com>
2017-06-18 13:09:18 +0300

    removed compiled js outputs

--- client-web/src/app/blink-item.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var BlinkItem = (function () {
-    function BlinkItem() {
-        this.tags = [];
-    }
-    return BlinkItem;
-}());
-exports.BlinkItem = BlinkItem;
